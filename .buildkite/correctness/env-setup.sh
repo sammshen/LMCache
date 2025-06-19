ff4dfc462f0935508a6cebbb1496c96c6aeb1657
Samuel Shen <slshen@tensormesh.ai>
2025-06-19 08:46:50 +0000

    fix dependencies
    Signed-off-by: Samuel Shen <slshen@tensormesh.ai>

--- .buildkite/correctness/env-setup.sh
+++ .buildkite/correctness/env-setup.sh
@@ -22,15 +22,15 @@ fi
 
 echo "Latest version of pre-release lmcache found: $latest_version"
 
+# Extra dependencies needed for the MMLU scripts
+pip install requests pandas numpy tqdm json matplotlib fastapi
+
 pip install vllm
 # Install latest pre-release wheel of lmcache
 # the lmcache wheel also gives us access to:
 # lmcache_server entrypoint
 # lmcache_controller entrypoint
 pip install --index-url https://pypi.org/simple --extra-index-url https://test.pypi.org/simple lmcache==$latest_version
-
-# Extra dependencies needed for the MMLU scripts
-pip install requests pandas numpy tqdm json matplotlib
 
 # Download the MMLU dataset
 wget -q --show-progress https://people.eecs.berkeley.edu/~hendrycks/data.tar
